7c1c54054af4d88d6d58182280faf73af29da876
Ruan <ruanpontesfrazao@gmail.com>
2023-10-22 23:48:36 -0300

    atualizando

--- script.js
+++ script.js
@@ -1,39 +1,27 @@
 const inputArquivo = document.querySelector('#arquivo');
 const listaArquivos = document.querySelector('#tabelaArquivos');
+const uploadCard = document.querySelector('#upload-card'); // Adicione esta linha
 
+uploadCard.addEventListener('click', function () { // Adicione este bloco
+    inputArquivo.click();
+});
+    
 inputArquivo.addEventListener('change', function () {
     for (const file of this.files) {
         const row = document.createElement('tr');
-        const cell = document.createElement('td');
-        cell.textContent = file.name;
-        row.appendChild(cell);
+        const cellNome = document.createElement('td');
+        cellNome.textContent = file.name;
+        cellNome.className = 'nome-arquivo';
+        const cellDownload = document.createElement('td');
+        cellDownload.className = 'download-cell';
+        const downloadLink = document.createElement('a');
+        downloadLink.href = URL.createObjectURL(file);
+        downloadLink.download = file.name;
+        downloadLink.textContent = 'Download';
+        downloadLink.className = 'download-button';
+        cellDownload.appendChild(downloadLink);
+        row.appendChild(cellNome);
+        row.appendChild(cellDownload);
         listaArquivos.appendChild(row);
     }
-});
-
-
-
-const download = function () {
-    return function (conteudos, nomesArquivos) {
-        for (let i = 0; i < conteudos.length; i++) {
-            const conteudo = conteudos[i];
-            const nomeArquivo = nomesArquivos[i];
-            const a = document.createElement('a');
-            a.style.display = 'none';
-            document.body.appendChild(a);
-
-            const blob = new Blob([conteudo], { type: 'octet/stream' });
-            const url = window.URL.createObjectURL(blob);
-            a.href = url;
-            a.download = nomeArquivo;
-            a.click();
-            window.URL.revokeObjectURL(url);
-        }
-    };
-};
-
-btnDownload.addEventListener('click', function () {
-    const conteudos = ['Conteúdo do arquivo 1', 'Conteúdo do arquivo 2']; // Adicione os conteúdos dos arquivos aqui
-    const nomesArquivos = ['arquivo1.txt', 'arquivo2.txt']; // Adicione os nomes dos arquivos aqui
-    download()(conteudos, nomesArquivos);
 });
